ad16f8e4a880af2d981145f8b93a4ca1d9fb41c6
Omar Mohammed <om17102@auis.edu.krd>
2023-05-23 17:21:50 +0300

    adding features of the movie

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -3,11 +3,20 @@
     <div class="w-1/2">
       <transition name="fade" mode="out-in">
         <movie v-if="details" :movie="details"></movie>
+
         <span v-else>Loading Details...</span>
       </transition>
+      <div v-if="details" class="mt-4">
+        <h1 class="text-2xl font-bold">{{ details.title }}</h1>
+        <p class="mb-2">Release Date: {{ formatDate(details.releaseDate) }}</p>
+        <p class="mb-2">Genre: {{ getGenreNames() }}</p>
+        <p class="mb-2">Rating: {{ details.vote_average }}</p>
+        <p class="mb-2">Popularity: {{ details.popularity }}</p>
+      </div>
     </div>
   </div>
 </template>
+
 <script setup>
 import axios from "@/http";
 import { useRoute } from "vue-router";
@@ -19,6 +28,21 @@ const route = useRoute();
 const fetchMovieDetails = async () => {
   const { data } = await axios.get(`/movie/${route.params.id}`);
   details.value = data;
+};
+const getGenreNames = () => {
+  if (details.value && details.value.genres) {
+    return details.value.genres.map((genre) => genre.name).join(", ");
+  }
+  return "Not Available";
+};
+
+const formatDate = (date) => {
+  const releaseDate = new Date(date);
+  return releaseDate.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  });
 };
 onMounted(() => {
   fetchMovieDetails();
